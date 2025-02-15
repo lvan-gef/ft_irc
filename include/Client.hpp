d732fbec934abf7cf0d5d025dedf24312382ac9a
luuk <lvan-gef@student.codam.nl>
2025-02-16 00:55:14 +0100

    Create the definitions for the client

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,9 +1,13 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include <ctime>
+#include <string>
+#include <sys/epoll.h>
+
 class Client {
   public:
-    Client();
+    explicit Client(int fd);
 
     Client(const Client &rhs);
     Client &operator=(const Client &rhs);
@@ -12,6 +16,34 @@ class Client {
     Client &operator=(Client &&rhs) noexcept;
 
     ~Client();
+
+  public:
+    int getFD() const noexcept;
+    const std::string &getUsername() const noexcept;
+    const std::string &getNickname() const noexcept;
+    epoll_event &getEvent() noexcept;
+    bool isRegistered() const noexcept;
+    time_t getLastSeen() const noexcept;
+
+  public:
+    void setUsername(const std::string &username) noexcept;
+    void setNickname(const std::string &nickname) noexcept;
+    void setRegistered(bool registered) noexcept;
+    void updatedLastSeen() noexcept;
+
+  public:
+    void appendToBuffer(const std::string &data) noexcept;
+    std::string getAndClearBuffer() noexcept;
+    bool hasCompleteMessage() const noexcept;
+
+  private:
+    int _fd;
+    std::string _username;
+    std::string _nickname;
+    std::string _partial_buffer;
+    epoll_event _event;
+    time_t _last_seen;
+    bool _registered;
 };
 
 #endif // !CLIENT_HPP
