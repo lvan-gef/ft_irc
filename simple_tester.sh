85bbb63ac7b8e26bbfd5840a8f78963681cddaf2
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-05-10 22:16:20 +0200

    Format code

--- simple_tester.sh
+++ simple_tester.sh
@@ -4,7 +4,7 @@
 # a simple tester to make sure we dont segfault any more of we dont have everything we expect...
 
 if [ "$(uname)" = "Darwin" ]; then
-	IRC_SERVER=$(ifconfig | grep "inet " | grep 127.0.0.1 | awk '{print $2}')
+    IRC_SERVER=$(ifconfig | grep "inet " | grep 127.0.0.1 | awk '{print $2}')
 else
     IRC_SERVER=$(hostname -I | awk '{print $1}')
 fi
@@ -15,72 +15,72 @@ NICK="luuk"
 USER1="hello"
 CHANNEL="#nc"
 {
-	sleep 0.5
+    sleep 0.5
 
-	echo > /dev/tty;
-	echo "PASS";
-	echo "NICK $NICK";
-	echo "USER $NICK * $IRC_SERVER :$USER1";
-	sleep 0.5;
+    echo > /dev/tty;
+    echo "PASS";
+    echo "NICK $NICK";
+    echo "USER $NICK * $IRC_SERVER :$USER1";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK";
-	echo "USER $NICK * $IRC_SERVER :$USER1";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK";
+    echo "USER $NICK * $IRC_SERVER :$USER1";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK 1$NICK";
-	echo "USER $NICK * $IRC_SERVER :$USER1";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK 1$NICK";
+    echo "USER $NICK * $IRC_SERVER :$USER1";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK 1";
-	echo "USER $NICK * $IRC_SERVER :$USER1";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK 1";
+    echo "USER $NICK * $IRC_SERVER :$USER1";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER $NICK";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER $NICK";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER $NICK * ";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER $NICK * ";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER $NICK * $IRC_SERVER";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER $NICK * $IRC_SERVER";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER $NICK * $IRC_SERVER :";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER $NICK * $IRC_SERVER :";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER [$NICK * $IRC_SERVER :$USER1";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER [$NICK * $IRC_SERVER :$USER1";
+    sleep 0.5;
 
-	echo "PASS $PASSWORD";
-	echo "NICK $NICK";
-	echo "USER $NICK * $IRC_SERVER :$USER1";
-	sleep 0.5;
+    echo "PASS $PASSWORD";
+    echo "NICK $NICK";
+    echo "USER $NICK * $IRC_SERVER :$USER1";
+    sleep 0.5;
 
     echo "JOIN";
-	sleep 0.5;
+    sleep 0.5;
 
     echo "JOIN #";
-	sleep 0.5;
+    sleep 0.5;
 
     echo "JOIN #test";
-	sleep 0.5;
+    sleep 0.5;
 
     echo "TOPIC";
     sleep 0.5;
